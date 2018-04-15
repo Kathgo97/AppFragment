d30642fdd1f8c851c295279e49cdc7f01e991d54
Douglas_HdezT <00004516@uca.edu.sv>
2018-04-15 14:40:15 -0600

    Landscape complete

--- app/src/main/java/com/debugprojects/fragapp/PersonViewer.java
+++ app/src/main/java/com/debugprojects/fragapp/PersonViewer.java
@@ -9,26 +9,52 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+import de.hdodenhof.circleimageview.CircleImageView;
+import static com.debugprojects.fragapp.ListViewer.FRAG_KEY;
 
 /**
  * Created by Douglashdezt
  */
 public class PersonViewer extends Fragment {
 
+    private CircleImageView circleImage;
+    private TextView text_name;
+    private TextView text_mail;
+    private TextView text_career;
+    private Button button_share;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.person_viewer, container, false);
 
-        Bundle bundle = this.getArguments();
+        getAllViews(view);
 
+        Bundle bundle = this.getArguments();
+        Persona p1;
 
         if(bundle != null){
             //Toast.makeText(getActivity(), "Item: " + bundle.getString("KEY"), Toast.LENGTH_SHORT).show();
+            p1 = (Persona) bundle.getSerializable(FRAG_KEY);
+
+            circleImage.setImageResource(p1.getImage_resource_id());
+            text_name.setText(p1.getFullName());
+            text_career.setText(p1.getCareer_var());
+            text_mail.setText(p1.getMail_var());
 
         }
 
         return view;
+    }
+
+    private void getAllViews(View v){
+        circleImage = v.findViewById(R.id.person_image);
+        text_name = v.findViewById(R.id.text_view_name);
+        text_mail = v.findViewById(R.id.text_view_mail);
+        text_career = v.findViewById(R.id.text_view_career);
+        button_share = v.findViewById(R.id.button_share);
     }
 }
